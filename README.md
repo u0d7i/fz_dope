15f313c86171f61aed37fa3263fe740c1b168231
u0d7i <u0d7i@users.noreply.github.com>
2023-07-30 00:33:59 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## fz_dope
+
+Simple Flipper Zero app for resetting [dolphin](https://docs.flipperzero.one/basics/dolphin) state - just removes /int/.dolphin.state file and reboots.
+
+App name is a reference to the substances, making [Jones](https://williamgibson.fandom.com/wiki/Jones) happy again.
+
+![Screenshot](assets/screenshot.png)
---
+++ fz_dope.c
@@ -0,0 +1,62 @@
+#include <furi.h>
+#include <gui/gui.h>
+#include <power/power_service/power.h>
+#include <storage/storage.h>
+
+static void fz_dope_draw_callback(Canvas * canvas, void *ctx) {
+    UNUSED(ctx);
+
+    canvas_clear(canvas);
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 0, 7, "fz-dope");
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str(canvas, 13, 25, "RESET dolphin state?");
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 0, 51, "[ok] to reset and reboot");
+    canvas_draw_str(canvas, 0, 63, "[back] to exit");
+}
+
+static void fz_dope_input_callback(InputEvent * input_event, void *ctx) {
+    furi_assert(ctx);
+    FuriMessageQueue *event_queue = ctx;
+
+    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
+}
+
+int32_t fz_dope(void *p) {
+    UNUSED(p);
+
+    InputEvent event;
+    FuriMessageQueue *event_queue =
+	furi_message_queue_alloc(8, sizeof(InputEvent));
+
+    ViewPort *view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, fz_dope_draw_callback, NULL);
+    view_port_input_callback_set(view_port, fz_dope_input_callback,
+				 event_queue);
+
+    Gui *gui = furi_record_open(RECORD_GUI);
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+
+    while (1) {
+	furi_check(furi_message_queue_get
+		   (event_queue, &event, FuriWaitForever) == FuriStatusOk);
+
+	if (event.key == InputKeyBack) {
+	    break;
+	}
+	if (event.key == InputKeyOk) {
+	    Storage *storage = furi_record_open(RECORD_STORAGE);
+	    storage_simply_remove(storage, INT_PATH(".dolphin.state"));
+	    power_reboot(PowerBootModeNormal);
+	}
+    }
+
+    furi_message_queue_free(event_queue);
+
+    gui_remove_view_port(gui, view_port);
+    view_port_free(view_port);
+    furi_record_close(RECORD_GUI);
+
+    return 0;
+}
